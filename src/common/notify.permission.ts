e72fc91ba2d59ab4f98e046afeef95610d01c02a
PhamAnhTuan <tuanpa.nkaitech@gmail.com>
2025-08-14 13:17:32 +0700

    update

--- src/common/notify.permission.ts
+++ src/common/notify.permission.ts
@@ -8,17 +8,15 @@ export function canViewNotify(user: User, notify: any): boolean {
 
   if (notify.level === "branch") {
     if (user.role === "admin") return true;
-    if (["staff", "branch-admin"].includes(user.role) && sameBranch)
-      return true;
+    if (["branch-admin", "staff"].includes(user.role) && sameBranch) return true; 
   }
 
   if (notify.level === "user" && sameUser) return true;
 
   if (notify.type === "food") {
-    if (["branch-admin", "staff"].includes(user.role) && sameBranch)
-      return true;
-    if (user.role === "admin") return true;
-    if (user.role === "user" && sameUser) return true;
+    if (["branch-admin", "staff"].includes(user.role) && sameBranch && notify.level === "branch") return true;
+    if (user.role === "user" && sameUser && notify.level === "user") return true;
+    if (user.role === "admin" && notify.level === "branch") return true;
   }
 
   return false;
